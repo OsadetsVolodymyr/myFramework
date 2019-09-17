142c635ed4d34395dd71dfe8ee1e9c551e1c550a
Volodymyr Osadets <osadetsvova09@gmail.com>
2019-09-17 18:38:14 +0300

    Finish framework

---
+++ myFramework/myFramework/Service.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func soneFunc() -> String {
+        return "Some Text"
+    }
+}
---
+++ myFramework/myFramework/myFramework.h
@@ -0,0 +1,19 @@
+//
+//  myFramework.h
+//  myFramework
+//
+//  Created by Volodymyr Osadets on 9/17/19.
+//  Copyright © 2019 Volodymyr Osadets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myFramework.
+FOUNDATION_EXPORT double myFrameworkVersionNumber;
+
+//! Project version string for myFramework.
+FOUNDATION_EXPORT const unsigned char myFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myFramework/PublicHeader.h>
+
+
